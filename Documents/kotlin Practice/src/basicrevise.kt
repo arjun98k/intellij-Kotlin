454a65b89705c342100657e3e1e36889a0b28ca5
arjun98k <arjunkandekar14@gmail.com>
2025-06-11 16:14:03 +0530

    basic revise

--- Documents/kotlin Practice/src/basicrevise.kt
+++ Documents/kotlin Practice/src/basicrevise.kt
@@ -3,4 +3,15 @@ fun main() {
 
     var salary:Int = 1200
     println(salary)
+
+    val value2: Long = 98
+
+   for (i in 2..3){
+       println(i)
+   }
+    for (i in 1..10){
+        println(i)
+    }
+
 }
+
