65b26db3003af96b0feece6638c6a763620af1ad
arjun98k <arjunkandekar14@gmail.com>
2025-06-12 18:53:10 +0530

    top level

---
+++ Documents/kotlin Practice/src/Datatype.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // Boolean type
+    val flag: Boolean = true
+    println(flag)
+    //charcter
+    val ah: Char = 'B'
+    println(ah)
+    //Arrays
+    var arr =arrayOf<Int>(11,12,13,15,16)
+    for (i in arr ) {
+        println(i  )
+    }
+}
+
