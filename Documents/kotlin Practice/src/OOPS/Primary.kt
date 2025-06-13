b884c93dd27806408669cb5e04b865f7febc4674
arjun98k <arjunkandekar14@gmail.com>
2025-06-13 18:24:16 +0530

    primary constructor and init block

--- Documents/kotlin Practice/src/OOPS/Primary.kt
+++ Documents/kotlin Practice/src/OOPS/Primary.kt
@@ -2,13 +2,19 @@ package OOPS
 
 
     class Primary(val name: String , var age:Int){
-
+   init {
+       if (age > 18){
+           println("his name ${name}")
+       }
+   }
     }
 
 
 fun main(){
     val primary = Primary("Arjun",20)
-    println("name of Og dev " + primary.name)
-    println("age  of ${primary.name}  is ${primary.age}")
+    val primary2 = Primary("aditya",23)
+
+//    println("name of Og dev " + primary.name)
+//    println("age  of ${primary.name}  is ${primary.age}")
 
 }
