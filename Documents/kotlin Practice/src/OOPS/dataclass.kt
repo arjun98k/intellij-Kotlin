c0b5a738104851e0e1a9fc19408b9df16085b58f
arjun98k <arjunkandekar14@gmail.com>
2025-06-14 19:09:15 +0530

    dataclass in kotlin

--- Documents/kotlin Practice/src/OOPS/dataclass.kt
+++ Documents/kotlin Practice/src/OOPS/dataclass.kt
@@ -4,8 +4,11 @@ data class dataclass(val name: String,val age: Int) {
 }
 
 fun main(){
-    val obj1 = dataclass("Arjun",24)
+    val obj1 = dataclass("Arjun",22)
     val obj2 = dataclass("Aditya",22)
     println(obj1)
     println(obj1.name == obj2.name)
+    println("are there is age is same " + obj1.age.compareTo(obj2.age) )
+    println(obj1.age == obj2.age)  // true or false
+
 }
