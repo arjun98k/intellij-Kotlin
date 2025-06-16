06f4d05035a3e799591158457e8a63b2b9f7a9b5
arjun98k <arjunkandekar14@gmail.com>
2025-06-16 20:59:06 +0530

    Generics in kt

--- Documents/kotlin Practice/src/OOPS/Generics1.kt
+++ Documents/kotlin Practice/src/OOPS/Generics1.kt
@@ -1,16 +1,24 @@
 package OOPS
 
-class Generics1<T> (age:T) {
-    var age:T = age
+class Generics1<T> (input:T) {
+    var age:T
     init {
-        this.age = age
+        age = when (input){
+            is Double -> kotlin.math.round(input) as T
+            is Float -> kotlin.math.round(input).toFloat() as T
+            is Int -> input // already rounded
+            is Long -> input
+            else -> input // non-number types
+        }
         println(age)
+
     }
 }
 
 fun main(){
     var obj1: Generics1<Int> = Generics1<Int>(21)
     var obj2: Generics1<String> = Generics1<String>("27")
+    var obj3: Generics1<Double> = Generics1<Double>(22.01)
 
 
 }
